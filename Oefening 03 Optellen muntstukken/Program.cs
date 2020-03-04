e4773ef4283f5ffd00ab6195bd462205cee2d734
Tim Janssens <timjanssens81@gmail.com>
2020-03-04 21:28:22 +0100

    Oefening03 omzetten data muntstukken

---
+++ Oefening 03 Optellen muntstukken/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Oefening_03_Optellen_muntstukken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Aantal muntstukken van 2 Euro?: ");
+            int muntenTweeEuro = int.Parse(Console.ReadLine());
+
+            Console.Write("Aantal munten van 1 euro?: ");
+            int muntenEenEuro = int.Parse(Console.ReadLine());
+
+            int totaalBedrag = muntenEenEuro * 1 + muntenTweeEuro * 2;
+
+            Console.WriteLine($"Het totale bedrag is {totaalBedrag}");
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
