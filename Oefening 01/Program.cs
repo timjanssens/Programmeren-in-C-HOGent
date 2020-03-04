6f00eec82cd61c965fd907b1c3208fd1ed5e5e42
Tim Janssens <timjanssens81@gmail.com>
2020-03-04 21:03:56 +0100

    Oefening01 en 02, Werken met data

---
+++ Oefening 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oefening_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int lengteInCm = 985;
+            Console.WriteLine($"Lengte in cm: {lengteInCm}");
+            //Uncomment onderstaande en vul de ... aan:            
+            
+            int aantalResterendeCm = lengteInCm;
+            int teller = 0;
+
+            while (aantalResterendeCm >= 100)
+            {
+                aantalResterendeCm -= 100;
+                teller++;
+            }
+
+
+            int volledigAantalMeters = teller;
+
+            Console.WriteLine($"Dit is {volledigAantalMeters}m en {aantalResterendeCm}cm.");     
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Oefening 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Oefening_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hoogteCilinderInMeter = 2.5;
+            double diamterCilinderInMeter = 2.1;
+            double opervlakteCilinderInMeter = Math.Pow(diamterCilinderInMeter, 2) * Math.PI / 4;
+            double inhoudCilinder = opervlakteCilinderInMeter * hoogteCilinderInMeter * 1000;
+
+            Console.WriteLine($"Inhoud in liter: {inhoudCilinder}");
+
+            Console.ReadKey();
+        }
+    }
+}
