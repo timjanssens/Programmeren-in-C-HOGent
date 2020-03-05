054c4dac7e219101be5021d9d0ba37e75a9a0fb3
Tim Janssens <timjanssens81@gmail.com>
2020-03-05 18:29:28 +0100

    Oefening 4 Bereken BMI

---
+++ Oefening 04 Berekenen BMI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_04_Berekenen_BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Lengte in cm?: "); 
+            double lengteInCm = double.Parse(Console.ReadLine());
+
+            Console.Write("Gewicht in kg?: "); 
+            int gewichtInKg = int.Parse(Console.ReadLine());
+
+            double bmi = gewichtInKg / Math.Pow(lengteInCm/100, 2);
+
+            Console.WriteLine($"BMI: {bmi}");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
