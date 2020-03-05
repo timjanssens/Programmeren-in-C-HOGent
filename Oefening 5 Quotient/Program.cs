f5b52fec951d3f76df1bc327e56425eb79afbd64
Tim Janssens <timjanssens81@gmail.com>
2020-03-05 18:48:54 +0100

    Oefening 5 Quotient

---
+++ Oefening 5 Quotient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Oefening_5_Quotient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Deeltal?: "); 
+            double deeltal = double.Parse(Console.ReadLine());
+
+            Console.Write("Deler?: "); 
+            double deler = double.Parse(Console.ReadLine());
+
+            if (deler == 0)
+            {
+                Console.WriteLine("Deling door nul wordt niet toegelaten");
+            }
+            else
+            {
+                double quotient = deeltal / deler;
+                Console.WriteLine($"Quotient: {quotient}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
