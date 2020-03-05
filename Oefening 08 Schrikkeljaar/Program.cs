28c4ba2870e34dcc8b0bc0c56f20c0084ba3269d
Tim Janssens <timjanssens81@gmail.com>
2020-03-05 19:37:50 +0100

    Oefening 07 Schrikkeljaar

---
+++ Oefening 08 Schrikkeljaar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_08_Schrikkeljaar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaartal: ");
+            int jaartal = int.Parse(Console.ReadLine());
+
+            if (jaartal % 4 == 0 && jaartal % 100 != 0 || jaartal % 400 == 0)
+            {
+                Console.WriteLine($"{jaartal} is een schrikkeljaar");
+            }
+            else
+            {
+                Console.WriteLine($"{jaartal} is geen schrikkeljaar");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
