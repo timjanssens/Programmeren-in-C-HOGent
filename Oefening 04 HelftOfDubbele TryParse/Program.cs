697eb36de8594541af6cce952ef6abb7e0ccb33d
Tim Janssens <timjanssens81@gmail.com>
2020-03-05 19:01:47 +0100

    Oefening 6 HelftOfDubbele TryParse

---
+++ Oefening 04 HelftOfDubbele TryParse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oefening_04_HelftOfDubbele_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Getal (int)?: ");
+            
+            bool geldigGetal = int.TryParse(Console.ReadLine(), out int getal);
+            if (geldigGetal)
+            {
+                if (getal % 2 == 0)
+                {
+                    Console.WriteLine($"Helft: {getal/2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Dubbele: {getal*2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Je mocht enkel een int invoeren");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
