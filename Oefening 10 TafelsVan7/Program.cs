160ac321bd7cf3aa77f3e88e588c8e1997a19ff4
Tim Janssens <timjanssens81@gmail.com>
2020-03-07 18:02:56 +0100

    Oefening 10 TafelsVan7 - 11 RobuusteInvoerDouble

---
+++ Oefening 10 TafelsVan7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_10_TafelsVan7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal = 7;
+            int factor = 1;
+
+            do
+            {
+
+                Console.WriteLine($"{factor} x {getal} = {factor * getal}");
+                factor++;
+
+            } while (factor <=10);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Oefening 11 RobuusteInvoerDouble/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_11_RobuusteInvoerDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double getal;
+            bool invoerOK;
+
+            do
+            {
+                Console.Write("Voer een (Double) getal in: ");
+                invoerOK = double.TryParse(Console.ReadLine(), out getal);
+
+            } while (invoerOK);
+
+
+            Console.WriteLine("Einde (wegens geen double getal).");
+
+            Console.ReadKey();
+        }
+    }
+}
