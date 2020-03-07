ed76cc37ab6a332511cca98bafb0d33ee0c226dd
Tim Janssens <timjanssens81@gmail.com>
2020-03-07 18:12:52 +0100

    Oefening 12 RobuusteInvoerDouble

---
+++ Oefening 12 RobuusteInvoerDouble/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Oefening_12_RobuusteInvoerDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double getal;
+            bool invoerOk = true;
+            Console.Write("Voer een (double) getal in?: ");
+
+            while (invoerOk)
+            {
+                invoerOk = double.TryParse(Console.ReadLine(), out getal);
+                if (invoerOk)
+                {
+                    Console.WriteLine("Dank je voor het (double) getal.");
+                    Console.Write("Gelieve nog een (double) getal in te voeren: ");
+                }
+                else
+                {
+                    Console.WriteLine("Einde (wegens geen double getal).");
+                }
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
