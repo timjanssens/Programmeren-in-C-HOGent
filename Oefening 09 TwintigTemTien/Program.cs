809d40f97acc9a0298958d70e784aa5355eee6a3
Tim Janssens <timjanssens81@gmail.com>
2020-03-07 17:48:47 +0100

    Oefening09 TwintiTemTien

---
+++ Oefening 09 TwintigTemTien/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_09_TwintigTemTien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal = 20;
+
+            do
+            {
+                if (getal % 2 == 0)
+                {
+                    Console.WriteLine(getal);
+                }
+                getal--;
+            } while (getal >= 10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
