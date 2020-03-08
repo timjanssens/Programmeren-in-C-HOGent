fcef9cd6aa2858da9769185c38994394499e2db0
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 15:39:18 +0100

    Oefening 27 GemeenteNamenEnPostcodes

---
+++ Oefening 27 GemeenteNamenEnPostcodes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Oefening_27_GemeenteNamenEnPostcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gemeenten = new string[8];
+            gemeenten[0] = "Brussel"; 
+            gemeenten[1] = "1000"; 
+            gemeenten[2] = "Antwerpen"; 
+            gemeenten[3] = "2000"; 
+            gemeenten[4] = "Brugge"; 
+            gemeenten[5] = "8000"; 
+            gemeenten[6] = "Gent"; 
+            gemeenten[7] = "9000";
+
+
+            for (int i = 1; i < gemeenten.Length; i += 2)
+            {
+                Console.WriteLine(gemeenten[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
