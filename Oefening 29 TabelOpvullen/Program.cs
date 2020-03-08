0e396782bf00098fc663326d7d51f79546a8bb09
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 15:58:23 +0100

    Oefening 29 TabelOpvullen

---
+++ Oefening 29 TabelOpvullen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_29_TabelOpvullen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] getallen = new int[9];
+            // opvullen:            
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                getallen[i] = 101 + i;
+            }
+            
+            
+            // afdrukken:            
+
+            for (int index = 0; index < getallen.Length; index++)
+                Console.Write(getallen[index] + " ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
