a337f32424eb91d40435779e507689980836a7e9
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 10:57:20 +0100

    Oefening 18 Vierkant

---
+++ Oefening 18 Vierkant/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Oefening_18_Vierkant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Zijde: ");
+            int zijde = int.Parse(Console.ReadLine());
+            int hoogte = 0;
+            do
+            {
+                int breedte = 0;
+                while (breedte <= zijde)
+                {
+                    Console.Write("*");
+                    breedte++;
+                }
+
+                Console.WriteLine();
+                hoogte++;
+
+            } while (hoogte <=  zijde);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
