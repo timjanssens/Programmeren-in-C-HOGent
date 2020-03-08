4bd199b33bc70b45d8a268cd8a466849499f0ebb
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 15:06:22 +0100

    Oefening 23 SecondenLijst

---
+++ Oefening 23 SecondenLijst/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Oefening_23_SecondenLijst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int seconden = 00; seconden < 60; seconden += 5)
+            {
+                Console.WriteLine($"23u59m{seconden.ToString("00")}s");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
