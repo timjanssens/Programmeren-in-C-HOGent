8b0fed655440715389748a4be6a511504d5ea440
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 08:44:18 +0100

    Oefening 14 Fibonacci

---
+++ Oefening 14 Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oefening_14_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Aantal maanden groei?: "); 
+            int maanden;
+            if (int.TryParse(Console.ReadLine(), out maanden) && maanden >= 1)
+            {
+                int fibo1 = 0; 
+                int fibo2 = 1; 
+                int fibo3 = fibo1 + fibo2;
+                int teller = 2;
+                
+
+                while (teller < maanden)
+                {
+                    fibo1 = fibo2;
+                    fibo2 = fibo3;
+                    fibo3 = fibo1 + fibo2;
+                    teller++;
+                }
+                                                                      
+                Console.Write($"Aantal knooppunten: {fibo3}");
+            }                
+                Console.ReadKey();
+        }
+    }
+}
