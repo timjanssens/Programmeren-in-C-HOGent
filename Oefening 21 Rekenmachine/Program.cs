38f98a8b14961aae90f25df0646db8ac603cf2bd
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 14:59:09 +0100

    Oefening 22 Rekenmachine

---
+++ Oefening 21 Rekenmachine/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Oefening_21_Rekenmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string bewerking = "+";
+            int uitkomst = 0;
+            int getal;
+            string invoer = "";
+
+            while (invoer != "=")
+            {
+                invoer = Console.ReadLine();
+                if (int.TryParse(invoer, out getal))
+                {
+
+                    if (bewerking == "-")
+                    {
+                        uitkomst -= getal;
+                    }
+
+                    if (bewerking == "+")
+                    {
+                        uitkomst += getal;
+                    }
+                }
+
+                else if (invoer == "+")
+                {
+                    bewerking = "+";
+                }
+                else if (invoer == "-")
+                {
+                    bewerking = "-";
+                }
+
+
+            }
+
+            Console.WriteLine($"{uitkomst}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
