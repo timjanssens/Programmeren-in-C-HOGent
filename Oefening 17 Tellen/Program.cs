9377c6b824dafba88c7ee3216e839abc9e4b5d7e
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 10:53:02 +0100

    Oefening 17 Tellen

---
+++ Oefening 17 Tellen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Oefening_17_Tellen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hoogte?: ");
+            int hoogte = int.Parse(Console.ReadLine());
+            int hoogteTeller = 0;
+
+            Console.Write("Breedte?: ");
+            int breedte = int.Parse(Console.ReadLine());
+
+
+            do
+            {
+
+                int breedteTeller = 0;
+
+                do
+                {
+                    Console.Write("*");
+                    breedteTeller++;
+                } while (breedteTeller < breedte);
+
+                Console.WriteLine();
+                hoogteTeller++;
+
+            } while (hoogteTeller < hoogte);
+
+                       
+            Console.ReadKey();
+        }
+    }
+}
