5aa9e7430837369a8e70288ab897f89fb9c599f5
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 10:42:49 +0100

    Oefening 16 ReeksVanKleinNaarGroot

---
+++ Oefening 16 ReeksVanKleinNaarGroot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Oefening_16_ReeksVanKleinNaarGroot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int getal1; 
+                Console.Write("Getal 1?: ");
+                while (!int.TryParse(Console.ReadLine(), out getal1))
+                    Console.Write("Gelieve een geheel getal in te voeren, getal 1?: ");
+
+                int getal2; 
+                Console.Write("Getal 2?: ");
+                while (!int.TryParse(Console.ReadLine(), out getal2))
+                    Console.Write("Gelieve een geheel getal in te voeren, getal 2?: ");
+
+                if (getal2 < getal1)
+                {
+                    int hulp = getal1;
+                    getal1 = getal2;
+                    getal2 = hulp;
+
+                }
+
+                Console.Write("Reeks van klein naar groot: ");
+
+                while (getal1 <= getal2)
+                {
+                    Console.Write(getal1 + " ");
+                    getal1++;
+                }
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
