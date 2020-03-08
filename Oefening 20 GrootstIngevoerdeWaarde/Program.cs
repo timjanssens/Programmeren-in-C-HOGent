c47d5257b31d241e5ca998766ff5afe64c6c8228
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 11:39:22 +0100

    Oefening 20 GrootstIngevoerdeWaarde

---
+++ Oefening 20 GrootstIngevoerdeWaarde/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Oefening_20_GrootstIngevoerdeWaarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal;
+            int grootste;
+
+
+            if (!int.TryParse(Console.ReadLine(), out getal))
+            {
+                Console.WriteLine("Er zijn geen getallen ingevoerd");
+            }
+
+            else
+            {
+
+                grootste = getal;
+
+
+                while (int.TryParse(Console.ReadLine(), out getal))
+                {
+
+                    if (getal > grootste)
+                    {
+                        grootste = getal;
+                    }
+
+                }
+
+                Console.WriteLine($"De grootst ingevoerde waarde is {grootste}"); ;
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
