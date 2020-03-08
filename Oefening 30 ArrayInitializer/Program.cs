4ba051e6b5aa938cb66788b8865a429e4028dd49
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 16:07:14 +0100

    Oefening 30 ArrayInitializer

---
+++ Oefening 30 ArrayInitializer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Oefening_30_ArrayInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dagen = new string[] {"ma", "di", "wo", "do", "vr", "za", "zo"};           
+            
+            Console.Write($"({dagen.Length} elementen): "); 
+
+            for (int index = 0; index < dagen.Length; index++) 
+                Console.Write(dagen[index] + " ");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
