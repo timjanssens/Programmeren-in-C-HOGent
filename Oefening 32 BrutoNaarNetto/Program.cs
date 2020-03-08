9a530714937e584280358bb35811b568e3b65584
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 16:14:00 +0100

    Oefening 32 BrutoNaarNetto

---
+++ Oefening 32 BrutoNaarNetto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Oefening_32_BrutoNaarNetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] kortingen = { 10m, 50m, 19.4m }; 
+            decimal brutoBedrag = 1000m;
+            decimal nettoBedrag = brutoBedrag;
+
+            foreach (var bedrag in kortingen)
+            {
+                nettoBedrag -= bedrag;
+            }
+           
+            Console.Write("Netto bedrag: " + nettoBedrag);
+
+
+            Console.ReadKey();
+        }
+    }
+}
