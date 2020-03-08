fd484996d15700817a0c38c6e6c5e8a32c477044
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 16:10:33 +0100

    Oefening 31 Foreach

---
+++ Oefening 31 Foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_31_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] getallen = new decimal[] { -5.1m, 0m, 10.4m, -15m };
+            foreach (var getal in getallen)
+            {
+
+                if (getal < 0)
+                    Console.WriteLine($"{getal} is negatief.");
+                else if (getal == 0m)
+                    Console.WriteLine($"{getal} is nul.");
+                else
+                    Console.WriteLine($"{getal} is positief.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
