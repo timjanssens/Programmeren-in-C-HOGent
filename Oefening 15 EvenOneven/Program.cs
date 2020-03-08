d0c01a4dd78267aac3a459825dd56f6f57fa74f0
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 09:14:49 +0100

    Oefening 15 EvenOneven

---
+++ Oefening 15 EvenOneven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_15_EvenOneven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 1;
+            //output wordt voor even gezet, niets bij even, on bij oneven.
+            string output = "";
+
+            while (getal <= 10)
+            {
+                output = (getal % 2 == 0) ? "" : "on";
+                Console.WriteLine($"{getal} is een {output}even getal.");
+                getal++;
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
