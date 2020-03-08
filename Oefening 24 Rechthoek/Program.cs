dfb2fbf53a57cb9d094e925c6c881533b2b86102
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 15:10:17 +0100

    Oefening 24 Rechthoek

---
+++ Oefening 24 Rechthoek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oefening_24_Rechthoek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hoogte?: "); 
+            int hoogte = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Breedte?: "); 
+            int breedte = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < hoogte; i++)
+            {
+                for (int j = 0; j < breedte; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
