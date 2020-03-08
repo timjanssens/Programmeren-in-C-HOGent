2b079e8532054a6e0dad5052f3efa09c3bcd1141
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 12:53:16 +0100

    Oefening 21 Som

---
+++ Oefening 21 Som/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oefening_21_Som
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal;
+
+            while (!int.TryParse(Console.ReadLine(), out getal))
+                Console.WriteLine("Gelieve een geheel getal in te voeren.");
+
+            int som = getal;
+
+            while (getal != 0)
+            {
+                Console.WriteLine("+");
+            
+                while (!int.TryParse(Console.ReadLine(), out getal))
+                    Console.WriteLine("Gelieve een geheel getal in te voeren.");
+
+                som += getal;
+                
+            }
+
+            Console.Write($"=\n{som}");
+            
+            Console.ReadKey();
+        }
+    }
+}
