808c6759af9b4cadba2e6f6119ae2140688c11d6
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 11:17:34 +0100

    Oefening 19 Driehoek

---
+++ Oefening 19 Driehoek/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Oefening_19_Driehoek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Rechthoekszijde: ");
+            int zijde = int.Parse(Console.ReadLine());
+            int hoogte = zijde;
+            int loopteller = 0;
+
+            while (hoogte > 0)
+            {
+
+                int breedteTeller = zijde - loopteller;
+
+                while (breedteTeller > 0)
+                {
+                    Console.Write("*");
+                    breedteTeller--;
+                }
+
+                Console.WriteLine();
+                hoogte--;
+                loopteller++;
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
