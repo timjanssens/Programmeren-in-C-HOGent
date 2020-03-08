030d5433ae53a788ba2091d2ec1671d2e634f6c3
Tim Janssens <timjanssens81@gmail.com>
2020-03-08 08:22:59 +0100

    Oefening 13 Permutaties

---
+++ Oefening 13 Permutaties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oefening_13_Permutaties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Aantal liedjes in de playlist?: ");
+            int aantalLiedjes;
+            if (int.TryParse(Console.ReadLine(), out aantalLiedjes) && aantalLiedjes >= 1)
+            {
+                int faculteit = 1;
+                int teller = 1;
+                while (teller <= aantalLiedjes)
+                {
+                    faculteit *= teller;
+                    teller++;
+
+                }
+
+
+
+                string meervoud = (faculteit > 1 ? "s" : "");
+                Console.Write($"{aantalLiedjes} liedje{meervoud} kan je in {faculteit} verschillende volgorde{meervoud} in een playlist plaatsen.");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
