03868e41d82bfcc0ffabeeed36fe67dab6ca24b8
Tim Janssens <timjanssens81@gmail.com>
2020-03-09 14:53:06 +0100

    Oefening 36 Zoekgeschiedenis

---
+++ Oefening 36 ZoekGeschiedenis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Oefening_36_ZoekGeschiedenis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] zoekGeschiedenis = { "Britney", "lamp vervangen", "arrays", "rijst koken", "nexus" };
+            do
+
+            {
+                // Afdrukkken lijst van recent geopende documenten:                
+
+                foreach (string zoekterm in zoekGeschiedenis)
+                    Console.Write(zoekterm + " ");
+                Console.WriteLine();
+
+                // Opvragen nieuw document:               
+
+                Console.Write("Nieuwe zoekterm: ");
+                string nieuweZoekterm = Console.ReadLine();
+                Console.WriteLine();
+
+                // Naar voor schuiven van laatste 4 elementen (eerste gaat verloren):                
+
+                for (int i = 0; i < zoekGeschiedenis.Length-1; i++)
+                {
+                    zoekGeschiedenis[i] = zoekGeschiedenis[i + 1];
+                }
+
+
+                // Invoegen nieuwe zoekterm (in laatste element):     
+                zoekGeschiedenis[4] = nieuweZoekterm;
+
+            } while (true);
+        }
+    }
+}
