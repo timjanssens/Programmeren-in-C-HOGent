74db78a0094025f279ddd219389c2a39cdb2eacc
Tim Janssens <timjanssens81@gmail.com>
2020-03-09 14:42:52 +0100

    Oefening 35 NamenTabel

---
+++ Oefening 35 NamenTabel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oefening_35_NamenTabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] voornamen = { "Jan", "Piet", "Pol" };
+            string[] familienamen = { "Jansens", "Pieters", "Polders" };
+
+            //Tabel namen instantiëren:            
+            string[] namen = new string[3];
+
+
+            //Tabel namen opvullen:  
+            for (int i = 0; i < namen.Length; i++)
+            {
+                namen[i] = voornamen[i] + " " + familienamen[i];
+            }
+
+
+            //Tabel afdrukken:
+
+            foreach (string naam in namen)
+                Console.WriteLine(naam);
+
+            Console.ReadKey();
+        }
+    }
+}
