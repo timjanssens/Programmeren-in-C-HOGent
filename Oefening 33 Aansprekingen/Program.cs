5deb7994f4f075f6b4611fbc722cb2477bf35203
Tim Janssens <timjanssens81@gmail.com>
2020-03-09 14:39:35 +0100

    Oefening 33 Aanspreking en Oefeing 34 Fibonacci

---
+++ Oefening 33 Aansprekingen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_33_Aansprekingen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] aansprekingen = { "Jan", "Piet", "Pol" };
+
+            for (int i = 0; i < aansprekingen.Length; i++)
+            {
+                aansprekingen[i] = "Dag " + aansprekingen[i];
+            }
+
+            foreach (string aanspreking in aansprekingen) 
+                Console.WriteLine(aanspreking);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Oefening 34 Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening_34_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[10];
+            fibonacci[0] = 1; 
+            fibonacci[1] = 1;
+            //overige elementen gelijkstellen aan som van de vorige twee ...
+            for (int i = 2; i < fibonacci.Length; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+
+
+            foreach (int getal in fibonacci) 
+                Console.Write(getal + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
