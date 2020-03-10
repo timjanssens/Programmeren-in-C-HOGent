89b2c21ab256dd8b1f291016c34b3297c9057a6d
Tim Janssens <timjanssens81@gmail.com>
2020-03-10 17:03:15 +0100

    Oefening 45 Morgen

---
+++ Oefening 45 Morgen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oefening_45_Morgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime morgen = Morgen();
+            Console.WriteLine(morgen);
+                                          
+            Console.ReadKey();
+        }
+
+        static DateTime Morgen()
+        {
+            DateTime morgen = DateTime.Now.AddDays(1);
+
+            return morgen;
+        }
+    }
+}
