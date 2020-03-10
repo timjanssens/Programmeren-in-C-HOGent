32d486aaf855ff5ff08e38313649f17c759caec0
Tim Janssens <timjanssens81@gmail.com>
2020-03-10 09:13:42 +0100

    Oefening 37 TemperatuurZoeken

---
+++ Oefening 37 TemperatuurZoeken/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Oefening_37_TemperatuurZoeken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] temperaturen = { 5, 11, 12, 32, 37 };
+            do
+            {
+                Console.Write("Temperatuur?: ");
+                int zoekwaarde = int.Parse(Console.ReadLine());
+
+                bool grotereTempGevonden = false;
+                int index = -1;
+
+                while (!grotereTempGevonden && index < temperaturen.Length - 1)
+                {
+
+                    index++;
+                    grotereTempGevonden = (temperaturen[index] > zoekwaarde);
+
+                }
+
+
+                //...
+
+                if (grotereTempGevonden)
+                {
+                    Console.WriteLine("Index eerste grotere temperatuur: " + index);
+                    Console.WriteLine("Eerste grotere temperatuur: " + temperaturen[index]);
+                }
+                else
+                    Console.WriteLine("Geen grotere temperatuur gevonden.");
+
+                Console.WriteLine();
+
+            } while (true);
+
+
+            Console.ReadKey();
+        }
+    }
+}
