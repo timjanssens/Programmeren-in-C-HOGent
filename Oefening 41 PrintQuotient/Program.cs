6245f7b5fd399f79ed9d02602a12af6d39ebfb05
Tim Janssens <timjanssens81@gmail.com>
2020-03-10 15:13:05 +0100

    Oefening 41 PrintQuotient

---
+++ Oefening 41 PrintQuotient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Oefening_41_PrintQuotient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintQuotient(10, 2.5);
+            PrintQuotient(9.9, 3);
+
+            Console.ReadKey();
+        }
+
+        static void PrintQuotient(double deeltal, double deler)
+        {
+            Console.WriteLine($"{deeltal} / {deler} = {(deeltal/deler).ToString("0.##")}");
+        }
+
+    }
+}
