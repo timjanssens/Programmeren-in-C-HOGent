79e45471f9f39dc6fa05f1ec82e92f1c782a0567
Tim Janssens <timjanssens81@gmail.com>
2020-03-10 15:53:33 +0100

    Oefening 43 DagenInFebruari

---
+++ Oefening 43 DagenInFebruari/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Oefening_43_DagenInFebruari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Jaar?: ");
+                int jaar = int.Parse(Console.ReadLine());
+                int dagen;
+                if (IsSchrikkeljaar(jaar))
+                {
+                    dagen = 29;
+                }
+                else
+                {
+                    dagen = 28;
+                }
+
+
+                    Console.WriteLine($"In februari van {jaar} zijn er {dagen} dagen.");
+                Console.WriteLine();
+            }
+            while (true);
+        }
+        //...        
+        static bool IsSchrikkeljaar(int jaartal)
+        {
+            return (jaartal % 400 == 0 || jaartal % 4 == 0 && jaartal % 100 != 0);
+        }
+    }
+
+}
