9a92501f1f9d5652e0960ac3c027f987cb4673fa
Tim Janssens <timjanssens81@gmail.com>
2020-03-14 14:30:21 +0100

    Oefening 54 Leeftijdscategorie

---
+++ Oefening 54 Leeftijdscategorie/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Oefening_54_Leeftijdscategorie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetCategorie(15) == Categorie.Jongere);      // zou true moeten geven            
+            Console.WriteLine(GetCategorie(16) == Categorie.Jongere);      // zou true moeten geven            
+            Console.WriteLine(GetCategorie(17) == Categorie.Normaal);      // zou true moeten geven            
+            Console.WriteLine(GetCategorie(59) == Categorie.Normaal);      // zou true moeten geven            
+            Console.WriteLine(GetCategorie(60) == Categorie.ZestigPlus);   // zou true moeten geven
+            Console.WriteLine(GetCategorie(61) == Categorie.ZestigPlus);   // zou true moeten geven
+            
+            Console.WriteLine(Toegangsprijs(Categorie.Jongere) == 10m);    // zou true moeten geven            
+            Console.WriteLine(Toegangsprijs(Categorie.ZestigPlus) == 12m); // zou true moeten geven           
+            Console.WriteLine(Toegangsprijs(Categorie.Normaal) == 19m);    // zou true moeten geven
+
+            Console.ReadKey();
+
+        }
+
+        static Categorie GetCategorie(int leeftijd)
+        {
+            if (leeftijd <= (int) Categorie.Jongere)
+            {
+                return Categorie.Jongere;
+            }
+            else if (leeftijd < (int) Categorie.Normaal)
+            {
+                return Categorie.Normaal;
+            }
+            else
+            {
+                return Categorie.ZestigPlus;
+            }
+        }
+        static decimal Toegangsprijs(Categorie categorie)
+        {
+            if (Categorie.Jongere == categorie)
+            {
+                return 10m;
+            }
+            else if (Categorie.Normaal == categorie)
+            {
+                return 19m;
+            }
+            else
+            {
+                return 12m;
+            }
+        }
+    }
+    enum Categorie
+    {
+        Jongere = 16, Normaal = 60, ZestigPlus
+    }
+}
