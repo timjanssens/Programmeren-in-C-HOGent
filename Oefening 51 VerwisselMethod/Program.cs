ae864a2dcaf8ca908d332baf6127b64faa6ec9ea
Tim Janssens <timjanssens81@gmail.com>
2020-03-14 11:14:15 +0100

    Oefening 51 VerwisselMethod

---
+++ Oefening 51 VerwisselMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Oefening_51_VerwisselMethod
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            int hoogte = 10; int breedte = 20;
+            Verwissel(ref hoogte,ref breedte);
+
+            Console.WriteLine(" Hoogte: " + hoogte);  // 20       
+            Console.WriteLine("Breedte: " + breedte); // 10
+            
+            Verwissel(ref hoogte,ref breedte);
+            
+            Console.WriteLine(" Hoogte: " + hoogte);  // 10           
+            Console.WriteLine("Breedte: " + breedte); // 20      
+
+            Console.ReadKey();
+        }
+        static void Verwissel(ref int hoogte, ref int breedte)
+        {
+            int hulp = hoogte;
+            hoogte = breedte;
+            breedte = hulp;
+        }
+    }
+}
