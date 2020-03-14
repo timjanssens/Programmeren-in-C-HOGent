158a562862d327c22c1920d3678768b62521835f
Tim Janssens <timjanssens81@gmail.com>
2020-03-14 14:37:21 +0100

    Oefening 55 toegangsprijs

--- Oefening 55 ToegangsPrijs/Program.cs
+++ Oefening 55 ToegangsPrijs/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Oefening_54_Leeftijdscategorie
+namespace Oefening_55_ToegangsPrijs
 {
     class Program
     {
@@ -12,10 +12,11 @@ namespace Oefening_54_Leeftijdscategorie
             Console.WriteLine(GetCategorie(59) == Categorie.Normaal);      // zou true moeten geven            
             Console.WriteLine(GetCategorie(60) == Categorie.ZestigPlus);   // zou true moeten geven
             Console.WriteLine(GetCategorie(61) == Categorie.ZestigPlus);   // zou true moeten geven
-            
-            Console.WriteLine(Toegangsprijs(Categorie.Jongere) == 10m);    // zou true moeten geven            
-            Console.WriteLine(Toegangsprijs(Categorie.ZestigPlus) == 12m); // zou true moeten geven           
-            Console.WriteLine(Toegangsprijs(Categorie.Normaal) == 19m);    // zou true moeten geven
+
+
+            Console.WriteLine(Toegangsprijs(Categorie.Jongere) == PrijsFormule.Korting);      // zou true moeten geven           
+            Console.WriteLine(Toegangsprijs(Categorie.ZestigPlus) == PrijsFormule.Korting);   // zou true moeten geven            
+            Console.WriteLine(Toegangsprijs(Categorie.Normaal) == PrijsFormule.Standaard);    // zou true moeten geven
 
             Console.ReadKey();
 
@@ -23,11 +24,11 @@ namespace Oefening_54_Leeftijdscategorie
 
         static Categorie GetCategorie(int leeftijd)
         {
-            if (leeftijd <= (int) Categorie.Jongere)
+            if (leeftijd <= (int)Categorie.Jongere)
             {
                 return Categorie.Jongere;
             }
-            else if (leeftijd < (int) Categorie.Normaal)
+            else if (leeftijd < (int)Categorie.Normaal)
             {
                 return Categorie.Normaal;
             }
@@ -36,24 +37,25 @@ namespace Oefening_54_Leeftijdscategorie
                 return Categorie.ZestigPlus;
             }
         }
-        static decimal Toegangsprijs(Categorie categorie)
+        static PrijsFormule Toegangsprijs(Categorie categorie)
         {
-            if (Categorie.Jongere == categorie)
+            if (Categorie.Normaal == categorie)
             {
-                return 10m;
+                return PrijsFormule.Standaard;
             }
-            else if (Categorie.Normaal == categorie)
-            {
-                return 12m;
-            }
+            
             else
             {
-                return 19m;
+                return PrijsFormule.Korting;
             }
         }
     }
     enum Categorie
     {
         Jongere = 16, Normaal = 60, ZestigPlus
+    }
+    enum PrijsFormule
+    {
+        Korting, Standaard
     }
 }
