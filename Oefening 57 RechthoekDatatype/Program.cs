08c1fa539a86cc05c0e67ce1fbd7d340aeee1b92
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 10:54:12 +0100

    Oefening 57 RechthoekDatatype

---
+++ Oefening 57 RechthoekDatatype/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_57_RechthoekDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rechthoek rechthoek1 = new Rechthoek();        
+            rechthoek1.SetHoogte(4);
+            rechthoek1.SetBreedte(8);         
+            Rechthoek rechthoek2 = new Rechthoek();        
+            rechthoek2.SetHoogte(5);        
+            rechthoek2.SetBreedte(10);    
+            PrintRechthoek(rechthoek1);     
+            PrintRechthoek(rechthoek2);
+
+            Console.ReadKey();
+        }
+        static void PrintRechthoek(Rechthoek r)
+        {    
+            Console.WriteLine($"Rechthoek met hoogte {r.GetHoogte()}, breedte {r.GetBreedte()} en oppervlakte {r.Oppervlakte()}.");
+        }
+    }
+}
---
+++ Oefening 57 RechthoekDatatype/Rechthoek.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_57_RechthoekDatatype
+{
+    class Rechthoek
+    {
+        public int Hoogte { get; set; }
+        public int Breedte { get; set; }
+
+        public void SetHoogte(int hoogte)
+        {
+            Hoogte = hoogte;
+        }
+        public void SetBreedte(int breedte)
+        {
+            Breedte = breedte;
+        }
+
+        public int GetHoogte()
+        {
+            return Hoogte;
+        }
+
+        public int GetBreedte()
+        {
+            return Breedte;
+        }
+
+        public int Oppervlakte()
+        {
+            return this.Breedte * this.Hoogte;
+        }
+
+
+    }
+}
