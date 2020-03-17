5bc67019eca44755fe789a6cc2ed274082032704
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 14:38:27 +0100

    Oefening 59 UitleningDatatype

---
+++ Oefening 58 UitleningDatatype/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Oefening_58_UitleningDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do           
+            {            
+                PrintUitleningen();
+
+
+                Console.Write("Nieuwe ontlening op: ");
+                DateTime d = DateTime.Parse(Console.ReadLine());
+                Console.Write("Omschrijving: ");
+                string o = Console.ReadLine();
+
+                Toevoegen(o, d);
+
+                Console.WriteLine();
+            }
+            while (true);
+
+            Console.ReadKey();
+
+        }
+
+
+
+
+
+
+
+        static Uitlening[] _uitleningen = new Uitlening[2];
+        static int _aantal = 0;
+
+        static void PrintUitleningen()        
+        {        
+            for (int index = 0; index < _aantal; index++)           
+            {          
+                Uitlening u = _uitleningen[index];        
+                Console.WriteLine($"- {u.GetOmschrijving()}: ontleent op {u.GetOntleendatum():dd/MM/yyyy} binnen ten laatste op {u.GetUitersteInleverdatum():dd/MM/yyyy}.");            
+            }         
+            Console.WriteLine();        
+        }
+        static void Toevoegen(string omschrijving, DateTime ontleendatum)
+        {
+            Uitlening nieuweUitlening = new Uitlening();
+            nieuweUitlening.SetOmschrijving(omschrijving);
+            nieuweUitlening.SetOntleendatum(ontleendatum);
+
+            _aantal++;        
+            if (_aantal > _uitleningen.Length) Array.Resize(ref _uitleningen, _uitleningen.Length * 2);
+            _uitleningen[_aantal - 1] = nieuweUitlening;
+        }
+
+    }
+
+}
---
+++ Oefening 58 UitleningDatatype/Uitlening.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_58_UitleningDatatype
+{
+    class Uitlening
+    {
+        static private DateTime _ontleendatum;
+        static private string _omschrijving;
+
+        public void SetOmschrijving(string omschrijving)
+        {
+            _omschrijving = omschrijving;
+        }
+        public string GetOmschrijving()
+        {
+            return _omschrijving;
+        }
+
+        public  void SetOntleendatum(DateTime ontleendatum)
+        {
+            _ontleendatum = ontleendatum;
+        }
+        public  DateTime GetOntleendatum()
+        {
+            return _ontleendatum;
+        }
+
+        public DateTime GetUitersteInleverdatum()
+        {
+            return _ontleendatum.AddDays(21);
+        }
+
+
+
+
+    }
+}
+
