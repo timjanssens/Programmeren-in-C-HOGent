9fc9323daf732f6ca1fbca4eb1f51e175ceb14b6
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 16:39:57 +0100

    Oefening 61 RechthoekProperties

---
+++ Oefening 61  RechthoekProperties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Oefening_61__RechthoekProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rechthoek rechthoek1 = new Rechthoek(); 
+            rechthoek1.Hoogte = 4.1; 
+            rechthoek1.Breedte = 8;
+            Rechthoek rechthoek2 = new Rechthoek();
+            rechthoek2.Hoogte =5;
+            rechthoek2.Breedte = 10.2;
+            PrintRechthoek(rechthoek1);
+            PrintRechthoek(rechthoek2);
+
+            Console.ReadKey();
+        }
+
+        static void PrintRechthoek(Rechthoek r) 
+        {
+            Console.WriteLine($"Rechthoek met hoogte {r.Hoogte}, breedte {r.Breedte} en oppervlakte {r.Oppervlakte}."); 
+        }
+
+    }
+}
---
+++ Oefening 61  RechthoekProperties/Rechthoek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_61__RechthoekProperties
+{
+    class Rechthoek
+    {
+        public double Breedte { get; set; }
+        public double Hoogte { get; set; }
+        private double oppervlakte;
+
+        public double Oppervlakte
+        {
+            get { return Breedte * Hoogte; }
+        }
+
+
+
+    }
+}
