6f7f582ecdf5abe83eb601218f8667151e56b338
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 16:22:29 +0100

    Oefening 60 Overschrijven

---
+++ Oefening 60  Overschrijven/Bankrekening.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_60__Overschrijven
+{
+    class Bankrekening
+    {
+        private decimal _saldo;
+        public void Stort(decimal bedrag) 
+        { 
+            _saldo = _saldo + bedrag; 
+        }
+        public void HaalAf(decimal bedrag)
+        { 
+            _saldo = _saldo - bedrag; 
+        }
+        public decimal Saldo()
+        { 
+            return _saldo; 
+        }        //...SchrijfOver... 
+
+        public void SchrijfOver(Bankrekening r2op, decimal bedrag)
+        {
+            r2op.Stort(bedrag);
+            this.HaalAf(bedrag);
+        }
+
+    }
+}
---
+++ Oefening 60  Overschrijven/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Oefening_60__Overschrijven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bankrekening b1 = new Bankrekening(); 
+            Bankrekening b2 = new Bankrekening();
+            decimal bedrag = 100m;
+            //Met één commando maak je het mogelijk 'bedrag' van 'b1' naar 'b2' over te schrijven:            
+            //...SchrijfOver...
+
+           b1.SchrijfOver(b2, 100);
+
+            Console.WriteLine(b1.Saldo() == -100m); // zou true moeten geven         
+            Console.WriteLine(b2.Saldo() == 100m);  // zou true moeten geven
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
