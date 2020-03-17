9a2b87ebf42f139ef71738665ac16adcbf08976f
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 11:20:23 +0100

    Oefening 57 ArtikelDatatype

---
+++ Oefening 58 ArtikelDatatype/Artikel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_58_ArtikelDatatype
+{
+    class Artikel
+    {
+
+        public double BtwTarief { get; set; } = 21;
+        public double PrijsZonderBtw { get; set; }
+
+        private double prijsMetBtw;
+
+        public double PrijsMetBtw
+        {
+            get { return PrijsZonderBtw + (PrijsZonderBtw * (BtwTarief/100)); }
+        }
+
+
+
+    }
+}
---
+++ Oefening 58 ArtikelDatatype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oefening_58_ArtikelDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Artikel artikel = new Artikel();
+            artikel.PrijsZonderBtw = 100;
+            Console.WriteLine(artikel.PrijsMetBtw);
+            Console.WriteLine(artikel.BtwTarief);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
