ee9937dc33fcec0300e30e1a8f55f7909969ebff
Tim Janssens <timjanssens81@gmail.com>
2020-03-17 10:13:01 +0100

    Oefening 56 uitlening

---
+++ Oefening 56 Uitlening/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Oefening_56_Uitlening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uitlening.SetOntleendatum(new DateTime(2018, 12, 30));            
+            Uitlening.SetMaximumTeOntlenenWeken(3);
+
+
+            Console.WriteLine($"Ontleendatum: {Uitlening.GetOntleendatum():dd/MM/yyyy}");            
+          Console.WriteLine($"Max. te ontlenen weken: {Uitlening.GetMaximumTeOntlenenWeken()}");            
+          Console.WriteLine($"Ten laatste inleveren op: {Uitlening.GetUitersteInleverdatum():dd/MM/yyyy}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Oefening 56 Uitlening/Uitlening.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_56_Uitlening
+{
+    class Uitlening
+    {
+        static private int _weken;
+        static private DateTime _ontleningsDatum;
+        public static void SetMaximumTeOntlenenWeken(int weken)
+        {
+            _weken = weken;
+        }
+        public static void SetOntleendatum(DateTime ontleningsdatum)
+        {
+            _ontleningsDatum = ontleningsdatum;
+        }
+
+        public static DateTime GetOntleendatum()
+        {
+            return _ontleningsDatum;
+        }
+        public static int GetMaximumTeOntlenenWeken()
+        {
+            return _weken;
+        }
+
+        public static DateTime GetUitersteInleverdatum()
+        {
+            DateTime terug = _ontleningsDatum.AddDays(7 * _weken);
+            return terug;
+        }
+
+
+
+
+    }
+}
