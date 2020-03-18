a335e9a0a5b1eacedabddd32e0870a8a3c71c83d
Tim Janssens <timjanssens81@gmail.com>
2020-03-18 08:57:15 +0100

    Oefening 62 PersoonProperties

---
+++ Oefening 62 PersoonProperties/Persoon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oefening_62_PersoonProperties
+{
+    class Persoon
+    {
+
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public string Rijksregisternummer { get; set; }
+        private string ingekorteNaam;
+
+        public string IngekorteNaam
+        {
+            get
+            {
+                return $"{Voornaam.Substring(0, 1)}. {Familienaam}";
+            }
+        }
+
+        public DateTime GeboorteDatum()
+        {
+            int jaar = int.Parse(Rijksregisternummer.Substring(0, 2));
+            if (jaar < 20)
+            {
+                jaar += 2000;
+            }
+            else
+            {
+                jaar += 1900;
+            }
+            int maand = int.Parse(Rijksregisternummer.Substring(2, 2));
+            int dag = int.Parse(Rijksregisternummer.Substring(4, 2));
+
+            return new DateTime(jaar, maand, dag);
+        }
+
+        public bool IsMan()
+        {
+            int geslacht = int.Parse(Rijksregisternummer.Substring(8, 1));
+            if (geslacht % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool IsVrouw()
+        {
+            int geslacht = int.Parse(Rijksregisternummer.Substring(8, 1));
+            if (geslacht % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int Leeftijd()
+        {
+            int jaren = 0;
+            DateTime GB = this.GeboorteDatum();
+            while (GB.AddYears(1) <= DateTime.Now)
+            {
+                jaren += 1; GB = GB.AddYears(1);
+            }
+            return jaren;
+
+        }
+
+
+    }
+}
---
+++ Oefening 62 PersoonProperties/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Oefening_62_PersoonProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon p1 = new Persoon();
+            p1.Voornaam = "Rita";
+            p1.Familienaam = "Peeters";
+            p1.Rijksregisternummer = "54041219067";
+            Console.WriteLine(p1.Voornaam == "Rita");                           // zou true moeten geven            
+            Console.WriteLine(p1.Familienaam == "Peeters");                     // zou true moeten geven           
+            Console.WriteLine(p1.IngekorteNaam == "R. Peeters");                // zou true moeten geven          
+            Console.WriteLine(p1.Rijksregisternummer == "54041219067");         // zou true moeten geven        
+            Console.WriteLine(p1.GeboorteDatum() == new DateTime(1954, 4, 12)); // zou true moeten geven        
+            Console.WriteLine(p1.IsMan() == false);                             // zou true moeten geven      
+            Console.WriteLine(p1.IsVrouw() == true);                            // zou true moeten geven              
+            int leeftijdP1 = Datum.AantalVolledigeJarenTotNuSinds(new DateTime(1954, 4, 12));
+            Console.WriteLine(p1.Leeftijd() == leeftijdP1);                     // zou true moeten geven
+
+            p1.Rijksregisternummer = "11030812112";
+            Console.WriteLine(p1.Voornaam == "Rita");                           // zou true moeten geven        
+            Console.WriteLine(p1.Familienaam == "Peeters");                     // zou true moeten geven          
+            Console.WriteLine(p1.IngekorteNaam == "R. Peeters");                // zou true moeten geven          
+            Console.WriteLine(p1.Rijksregisternummer == "11030812112");         // zou true moeten geven         
+            Console.WriteLine(p1.GeboorteDatum() == new DateTime(2011, 3, 8));  // zou true moeten geven          
+            Console.WriteLine(p1.IsMan() == true);                              // zou true moeten geven         
+            Console.WriteLine(p1.IsVrouw() == false);                           // zou true moeten geven          
+            leeftijdP1 = Datum.AantalVolledigeJarenTotNuSinds(new DateTime(2011, 3, 8));
+            Console.WriteLine(p1.Leeftijd() == leeftijdP1);                     // zou true moeten geven
+            p1.Rijksregisternummer = null;
+            Console.WriteLine(p1.Rijksregisternummer == null);                  // zou true moeten geven        
+            Console.WriteLine(p1.IsMan() == false);                             // zou true moeten geven      
+            Console.WriteLine(p1.IsVrouw() == false);                           // zou true moeten geven      
+                                                                                // Console.WriteLine(p1.Leeftijd() == 0);                              // zou true moeten geven
+
+            Console.ReadKey();
+        }
+    }
+    class Datum
+    {
+        public static int AantalVolledigeJarenTotNuSinds(DateTime d)
+        {
+            int jaren = 0;
+            while (d.AddYears(1) <= DateTime.Now)
+            {
+                jaren += 1; d = d.AddYears(1);
+            }
+            return jaren;
+        }
+    }
+}
